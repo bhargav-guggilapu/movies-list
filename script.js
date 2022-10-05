8e14dfe70084de13c7d60cf19bf43394efa31411
Bhargav Guggilapu <bhargavguggilapu@gmail.com>
2022-10-05 10:27:33 +0530

    added release date on hover

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 const currentYear = new Date().getFullYear();
 const startDate = `${currentYear}-01-01`;
 const endDate = `${currentYear}-12-31`;
-const API_URL = `https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${startDate}&primary_release_date.lte=${endDate}&api_key=d54294949aa5059e9f467e7dcab4ffe6`;
+const API_KEY = "&api_key=d54294949aa5059e9f467e7dcab4ffe6";
+const YEAR_MOVIES_URL = `/discover/movie?primary_release_date.gte=${startDate}&primary_release_date.lte=${endDate}`;
+const API_URL = "https://api.themoviedb.org/3";
 const IMAGE_URL = `https://image.tmdb.org/t/p/w500`;
 
 const moviesDiv = document.querySelector(".content");
 
-getMovies(API_URL);
+getMovies(API_URL + YEAR_MOVIES_URL + API_KEY);
+
 async function getMovies(url) {
   const response = await fetch(url);
   const data = await response.json();
+  const movies = data.results;
+  movies.sort(function (a, b) {
+    return new Date(b.release_date) - new Date(a.release_date);
+  });
 
-  data.results.forEach((movie) => {
-    const { title, poster_path, vote_average, overview, original_language } =
-      movie;
+  movies.forEach((movie) => {
+    const {
+      title,
+      poster_path,
+      vote_average,
+      original_language,
+      release_date,
+    } = movie;
     const code = `
     <div class="card">
         <div style="position: relative">
-          <img style="width: 100%;" src="${IMAGE_URL + poster_path}" />
+          <div style="position: relative; overflow: hidden">
+            <img style="width: 100%;" src="${IMAGE_URL + poster_path}" />
+            <span class="release-date">${release_date}</span>
+          </div>
           <div class="card-footer">
-              <h3 style="font-size: 15px">${title.toUpperCase()}</h3>
-              <span class="rating">${vote_average.toFixed(1)}</span>
+            <h3 style="font-size: 15px">${title.toUpperCase()}</h3>
+            <span class="rating">${vote_average.toFixed(1)}</span>
           </div>
           <span class="language">${original_language.toUpperCase()}</span>
         </div>
@@ -29,5 +44,3 @@ async function getMovies(url) {
     moviesDiv.innerHTML += code;
   });
 }
-
-//release_date: "2022-07-27", overview
